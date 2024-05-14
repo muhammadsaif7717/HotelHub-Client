deaee7389cc1c468dfcf54297a5482d2d39b9143
MuHammad Saif <muhammadsaif7717@gmail.com>
2024-05-15 03:30:11 +0600

    Booking Date Updating functionality added

--- src/Components/MyBookings/MyBookings.jsx
+++ src/Components/MyBookings/MyBookings.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Contexts/AuthProvider";
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
 
@@ -8,6 +8,9 @@ const MyBookings = () => {
     const bookings = useLoaderData();
     const { user } = useContext(AuthContext);
     const [myBookings, setMyBookings] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedBooking, setSelectedBooking] = useState(null);
+    const [newDate, setNewDate] = useState(null);
 
     useEffect(() => {
         const filteredBookings = bookings.filter(
@@ -16,7 +19,7 @@ const MyBookings = () => {
         setMyBookings(filteredBookings);
     }, [bookings, user.email]);
 
-    const handleCancleBooking = (id, deletingId) => {
+    const handleCancelBooking = (id, deletingId) => {
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
@@ -39,12 +42,10 @@ const MyBookings = () => {
                             });
                             
                             setMyBookings(prevBookings => prevBookings.filter(booking => booking._id !== id));
-
                         }
-                    })
+                    });
 
-                //update availability & bookedDate
-                // updates
+                // Update room availability & bookedDate
                 const updatedRoom = {
                     availability: "Yes",
                     bookedDate: " ",
@@ -53,35 +54,80 @@ const MyBookings = () => {
                 axios.patch(`http://localhost:5000/rooms/${deletingId}`, updatedRoom)
                     .then(res => {
                         console.log(res.data)
-                    })
-
+                    });
             }
         });
-    }
+    };
+
+    const handleUpdateDate = () => {
+        if (newDate && selectedBooking) {
+            // Update the booking date for the selected booking
+            const updatedBooking = { bookedDate: newDate };
+
+            axios.patch(`http://localhost:5000/bookings/${selectedBooking._id}`, updatedBooking)
+                .then(res => {
+                    console.log(res.data);
+                    // Close the modal and reset states
+                    setModalVisible(false);
+                    setSelectedBooking(null);
+                    setNewDate(null);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Failed to update booking date",
+                    });
+                });
+        }
+    };
 
     return (
-        <div className=" my-14">
+        <div className="my-14">
             <h2 className="font-bold text-3xl text-center mb-5">My Bookings: {myBookings.length}</h2>
-            <div className="grid grid-cols-1  lg:grid-cols-3 gap-10">
-                {myBookings.map(myBooking => (
-                    <div key={myBooking._id} className="bg-white shadow-lg w-full h-full rounded-lg overflow-hidden p-5 space-y-4">
-                        <img src={myBooking.selectedRoom.images[0]} alt={myBooking.selectedRoom.title} className="w-full  object-cover" />
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                {myBookings.map(booking => (
+                    <div key={booking._id} className="bg-white shadow-lg w-full h-full rounded-lg overflow-hidden p-5 space-y-4">
+                        <img src={booking.selectedRoom.images[0]} alt={booking.selectedRoom.title} className="w-full object-cover" />
                         <div className="">
-                            <div className="font-bold text-xl mb-4">{myBooking.selectedRoom.title}</div>
-                            <p className="text-gray-700 text-base">{myBooking.selectedRoom.description}</p>
+                            <div className="font-bold text-xl mb-4">{booking.selectedRoom.title}</div>
+                            <p className="text-gray-700 text-base">{booking.selectedRoom.description}</p>
                         </div>
                         <div className="">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">$ {myBooking.selectedRoom.pricePerNight}</span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Size: {myBooking.selectedRoom.roomSize}</span>
-
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">$ {booking.selectedRoom.pricePerNight}</span>
+                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Size: {booking.selectedRoom.roomSize}</span>
                         </div>
                         <div className="flex gap-5 items-center justify-start">
-                            <button onClick={() => handleCancleBooking(myBooking._id, myBooking.selectedRoom._id)} className="btn btn-error text-white border-none">Cancel</button>
-                            <Link className="btn btn-success  text-white bg-blue-400 border-none">Update Date</Link>
+                            <button onClick={() => handleCancelBooking(booking._id, booking.selectedRoom._id)} className="btn btn-error text-white border-none">Cancel</button>
+                            <button onClick={() => {
+                                setSelectedBooking(booking);
+                                setNewDate(booking.bookedDate); // Set default value to the currently selected booking date
+                                setModalVisible(true);
+                            }} className="btn btn-success  text-white bg-blue-400 border-none">Update Date</button>
                         </div>
                     </div>
                 ))}
             </div>
+
+            {/* Modal */}
+            {modalVisible && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+                    <div className="bg-white p-8 rounded-lg">
+                        <h3 className="text-lg font-semibold mb-4">Update Booking Date</h3>
+                        <input
+                            type="date"
+                            value={newDate}
+                            onChange={(e) => setNewDate(e.target.value)}
+                            className="input input-bordered mb-4"
+                        />
+                        <div className="flex justify-end mt-4">
+                            <button onClick={() => setModalVisible(false)} className="bg-gray-300 px-4 py-2 rounded-md mr-4">Close</button>
+                            <button onClick={handleUpdateDate} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Update Date</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
